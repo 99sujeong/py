3ba44666cc35240c4b9e1de41f45d48114cec87a
99sujeong <66475575+99sujeong@users.noreply.github.com>
2021-04-12 23:45:07 +0900

    update multiply-3

--- multiply-3.py
+++ multiply-3.py
@@ -1,8 +1,7 @@
 def multiply_three_numbers(x, y, z):
-    print(x * y * z)
-
+    return x * y * z
 
 # 테스트 코드
-multiply_three_numbers(7, 3, 5)
-multiply_three_numbers(21, 4, 9)
-multiply_three_numbers(-7, 6, 3)
+print(multiply_three_numbers(7, 3, 5))
+print(multiply_three_numbers(21, 4, 9))
+print(multiply_three_numbers(-7, 6, 3))
